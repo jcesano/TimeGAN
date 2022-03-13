7359e92ba429cf7d42bb5a5959ffec74f28773b2
jcesano <jcesano@gmail.com>
2022-03-13 16:43:42 -0300

    add preprocess. Problem with grouping
    grouping delete DATETIME column.

--- data/preprocess.py
+++ data/preprocess.py
@@ -2,7 +2,56 @@
 import pandas as pd
 import numpy as np
 
+def showing_missing_values_in_grouped_data(grouped_df):
+  print("Showing missing values (NaN values in grouped data.\n")  
+  showing_missing_values(grouped_df)
+
+def transforming_gaps_into_NaN_values(grouped_df, full_range):
+  print("PRUEBA \n")
+  print("Length of grouped_df before reindexing is:", len(grouped_df), ".\n")
+  print(grouped_df.head())
+  showing_missing_values(grouped_df)
+  grouped_df = grouped_df.reindex(full_range) 
+  print("LUEGO DE REINDEX \n")
+  print(grouped_df.head())
+  print("FIN PRUEBA \n")
+
+  return grouped_df
+
+def detecting_gaps_in_grouped_data(grouped_df):
+  print("Detecting gaps in DateTime column... \n")
+  
+  begin_date = grouped_df.index.min()
+  end_date =  grouped_df.index.max()
+  print("The begin date is:", begin_date, "\n")
+  print("The end date is:", end_date, "\n")
+
+
+  full_range = pd.date_range(start=begin_date, end=end_date, freq='H')
+
+  print("Full range begining is:\n")
+
+  print(full_range[:10], "\n")
+
+  full_range_len = len(full_range)
+  print("The full range has", full_range_len, "entries \n")
+
+  grouped_df_len = len(grouped_df)
+  print("The number of entries in grouped_df is:", grouped_df_len, "\n")
+
+  print("So we have", full_range_len - grouped_df_len, "entries that are missing to complete full date range \n")
+
+  print("The missing entries are the following: \n")
+
+  grouped_df_datetime = pd.to_datetime(grouped_df.index)
+
+  print(full_range.difference(grouped_df_datetime))
+
+  return full_range
+
 def grouping_by_date(original_df):
+
+  print("Grouping the dataset by DATETIME in order to have a unique datetime as key of the dataframe \n")
 
   return original_df.groupby('DATETIME').agg(
           Mean_S008=('S008', "mean"),    # Mean of sensor 008
@@ -13,6 +62,18 @@ def grouping_by_date(original_df):
           RecCount =('DATETIME', "count")   # get the count of networks            
         
         )
+def show_grouped_data_by_date(original_df):
+  
+  grouped_df = grouping_by_date(original_df)
+
+  print("New columns of grouped data are: \n")
+  print(grouped_df.columns)
+
+  print("\nShowing the first rows of the grouped dataset \n")
+  print(grouped_df.head())
+  
+  return grouped_df 
+
 def complete_col_NaN_values_with_last_valid_value(df_in, column_name):
   print("Completing NaN values in column: ",column_name, "\n")
 
@@ -41,7 +102,7 @@ def complete_NaN_values_with_last_valid_value(df_in):
 
   complete_col_NaN_values_with_last_valid_value(df_in, "S050")
 
-def transform_column_negative_in_NaN(df_in, column_name):
+def transform_column_negative_into_NaN(df_in, column_name):
   # Detecting negative numbers and turning them into NaN values
   print("Transforming column: ", column_name)
   cnt=0
@@ -57,49 +118,61 @@ def transform_column_negative_in_NaN(df_in, column_name):
 
 
 
-def transforming_negative_values_in_NaN(df_in):
+def transforming_negative_values_into_NaN(df_in):
   # Detecting negative numbers and turning them into NaN values
   print("Transforming in each column, each negative value into a NaN value \n")
 
-  transform_column_negative_in_NaN(df_in, 'S008')
+  transform_column_negative_into_NaN(df_in, 'S008')
 
-  transform_column_negative_in_NaN(df_in, 'S035')
+  transform_column_negative_into_NaN(df_in, 'S035')
 
-  transform_column_negative_in_NaN(df_in, 'S038')
+  transform_column_negative_into_NaN(df_in, 'S038')
 
-  transform_column_negative_in_NaN(df_in, 'S048')
+  transform_column_negative_into_NaN(df_in, 'S048')
 
-  transform_column_negative_in_NaN(df_in, 'S050')
+  transform_column_negative_into_NaN(df_in, 'S050')
 
 
 def showing_negative_values(df_num):
   print('Negatives Found: \n')
   print(df_num.where(df_num < 0).count())
+  print("\n")
 
 def showing_zeros_values(df_num):
   print('Zeros Found: \n')
   print(df_num.where(df_num == 0).count())
+  print("\n")
 
-def showing_missing_values(v_original_data):
+def showing_negative_zero_values(v_original_data):
+  print("Checking if we have any negative or zero values in the dataset.\n")
+  # select the float columns
+  df_num = v_original_data.select_dtypes(include=[np.float64])
+  
+  showing_negative_values(df_num)
+  showing_zeros_values(df_num)
+
+def showing_missing_values(v_current_data):
   print("Detecting missing values...\n")
-  print("Checking if there is any missing value in our dataset \n")
-  print(v_original_data.isnull().any())
-  print("\n Total missing values for each feature: \n")
-  print(v_original_data.isnull().sum())
-  print("\n Checking if we have any negative values in the dataset \n")
+  print("Checking if there is any missing value in our dataset.\n")
+  print(v_current_data.isnull().any())
+  print("\nTotal missing values for each feature: \n")
+  print(v_current_data.isnull().sum())
+  print("\n")
   
 def showing_shape_of_data(v_original_data):
   data_shape = v_original_data.shape
-  print(f"Original shape of Sensors data_frame: {data_shape} \n")
+  print(f"Shape of Sensors data_frame: {data_shape}. \n")
 
 
 def showing_head_of_data(v_original_data):
-  print("Showing first rows of table: \n")
+  print("Showing first rows of dataframe: \n")
   print(v_original_data.head())
+  print("\n")
 
 def showing_datatypes_of_data(v_original_data):
   print("Showing types of each column of dataframe: \n")
   print(v_original_data.dtypes)
+  print("\n")
 
 def show_data_features(v_original_data):
   
@@ -111,75 +184,46 @@ def show_data_features(v_original_data):
 
   showing_missing_values(v_original_data)
 
-  # select the float columns
-  df_num = v_original_data.select_dtypes(include=[np.float64])
-  
-  showing_negative_values(df_num)
-  showing_zeros_values(df_num)
-
+  showing_negative_zero_values(v_original_data)
   
   
 
 def open_input_file(file_name):  
-  print("Hello Julio")
-  
   # Load csv
   print("Loading data...\n")
   ori_data = pd.read_csv(file_name)
   
   return ori_data
 
+
+def preprocess_data_for_model():
+
+  file_name = "TimeGAN/data/NO2_sequence_five_sensors.csv"
+  original_df = open_input_file(file_name)
+
+  print("Showing some characteristics of the sensors data. \n")
+  show_data_features(original_df)
+
+  transforming_negative_values_into_NaN(original_df)
+
+  print("Showing the same characteristics after transformation of negative values into NaN values\n")
+  show_data_features(original_df)
+
+  complete_NaN_values_with_last_valid_value(original_df)
+
+  print("Showing the same characteristics of the sensors data after transformation of NaN values \n")
+  show_data_features(original_df)
+
+  grouped_df = show_grouped_data_by_date(original_df)
+
+  full_range = detecting_gaps_in_grouped_data(grouped_df)
+
+  grouped_df = transforming_gaps_into_NaN_values(grouped_df, full_range)
+
+  showing_missing_values_in_grouped_data(grouped_df)
+
 #########################
 # Load and preprocess data for model
 #########################
 
-file_name = "TimeGAN/data/NO2_sequence_five_sensors.csv"
-original_df = open_input_file(file_name)
-
-print("Showing some features of the sensors data \n")
-show_data_features(original_df)
-
-transforming_negative_values_in_NaN(original_df)
-
-print("Showing some features of the sensors data after transformation of negative values \n")
-show_data_features(original_df)
-
-complete_NaN_values_with_last_valid_value(original_df)
-
-print("Showing some features of the sensors data after transformation of NaN values \n")
-show_data_features(original_df)
-
-grouped_df = grouping_by_date(original_df)
-
-print("showing the first rows of the grouped dataset \n")
-print(grouped_df.head())
-
-print("Detecting gaps in DateTime column... \n")
-print(grouped_df.columns)
-
-begin_date = grouped_df.index.min()
-end_date =  grouped_df.index.max()
-print("The begin datetime of data is:", begin_date, "\n")
-print("The end datetime of data is:", end_date, "\n")
-
-
-full_range = pd.date_range(start=begin_date, end=end_date, freq='H')
-
-print("Full range begining is: \n")
-
-print(full_range[:10], "\n")
-
-full_range_len = len(full_range)
-print("The full range has the number of ", full_range_len, " entries \n")
-
-grouped_df_len = len(grouped_df)
-print("The number of entries in grouped_df is: ", grouped_df_len, "\n")
-
-print("So we have ", grouped_df_len - full_range_len, "entries that are missing to complete full date range \n")
-
-print("The missing entries are the following: \n")
-
-print(type(grouped_df))
-grouped_df_datetime = pd.to_datetime(grouped_df.index)
-
-print(full_range.difference(grouped_df_datetime))
+preprocess_data_for_model()
